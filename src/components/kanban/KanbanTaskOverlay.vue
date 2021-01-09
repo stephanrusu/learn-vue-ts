1aa8c2e1c17bd67623618485b3484575862e0b71
Stefan Rusu <stephan.rusu@gmail.com>
2021-01-09 18:16:01 +0200

    Update view task

--- src/components/kanban/KanbanTaskOverlay.vue
+++ src/components/kanban/KanbanTaskOverlay.vue
@@ -25,14 +25,58 @@
       appear
     > -->
     <section class="absolute inset-0 flex justify-end" aria-labelledby="slide-over-heading">
-      <div class="flex items-center cursor-pointer" @click="toggleFullScreen = !toggleFullScreen">
-        <div class="bg-white rounded-md w-2 h-32"></div>
+      <div class="flex items-center">
+        <router-link :to="{ name: 'kanban' }" class="bg-white rounded-md w-2 h-32"></router-link>
       </div>
       <div :class="`relative transition-all ease-in duration-300 ${toggleFullScreen ? 'w-p-95' : 'w-p-50'} p-2`">
         <div class="h-full flex flex-col bg-white rounded-md">
           <slot name="header"></slot>
           <slot name="main"></slot>
-          <slot name="footer"></slot>
+          <div class="flex flex-row items-center justify-between pb-4 px-4 mt-auto pt-3">
+            <button
+              type="button"
+              class="flex-0-auto px-3 py-2 hover:bg-gray-100 items-center justify-center rounded-md ease-in transition-colors"
+              @click="toggleFullScreen = !toggleFullScreen"
+            >
+              <svg
+                v-if="!toggleFullScreen"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather w-5 h-5 stroke-current text-gray-700"
+              >
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <polyline points="9 21 3 21 3 15"></polyline>
+                <line x1="21" y1="3" x2="14" y2="10"></line>
+                <line x1="3" y1="21" x2="10" y2="14"></line>
+              </svg>
+              <svg
+                v-else
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-minimize-2 w-5 h-5 stroke-current text-gray-700"
+              >
+                <polyline points="4 14 10 14 10 20"></polyline>
+                <polyline points="20 10 14 10 14 4"></polyline>
+                <line x1="14" y1="10" x2="21" y2="3"></line>
+                <line x1="3" y1="21" x2="10" y2="14"></line>
+              </svg>
+            </button>
+            <slot name="footer"></slot>
+          </div>
         </div>
       </div>
     </section>
--- src/components/kanban/task/KanbanTaskDisplay.vue
+++ src/components/kanban/task/KanbanTaskDisplay.vue
@@ -2,107 +2,30 @@
   <kanban-task-overlay>
     <template #header>
       <div class="p-4 bg-gray-50 rounded-tl-md rounded-tr-md shadow-small">
-        <div class="flex justify-between items-center pb-4">
-          <div class="flex flex-row items-center">
-            <div class="text-md font-medium text-gray-700 mr-4">{{ projectTitle }} - {{ task.uid }}</div>
-            <div class="relative inline-block text-left">
-              <div>
-                <button
-                  id="options-menu"
-                  type="button"
-                  class="inline-flex justify-center p-2 hover:bg-gray-50"
-                  aria-haspopup="true"
-                  aria-expanded="true"
-                  @click="taskOptionsToggle = !taskOptionsToggle"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    :class="`feather feather-more-horizontal w-5 h-5 stroke-current text-gray-700`"
-                  >
-                    <circle cx="12" cy="12" r="1"></circle>
-                    <circle cx="19" cy="12" r="1"></circle>
-                    <circle cx="5" cy="12" r="1"></circle>
-                  </svg>
-                </button>
-              </div>
-              <div
-                v-if="taskOptionsToggle"
-                class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
-              >
-                <div class="p-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                  <button
-                    class="rounded block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-800 text-left"
-                    role="menuitem"
-                    @click="taskOptionsToggle = !taskOptionsToggle"
-                  >
-                    Edit task
-                  </button>
-                  <button
-                    class="rounded block w-full px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-red-800 text-left"
-                    role="menuitem"
-                    @click="taskOptionsToggle = !taskOptionsToggle"
-                  >
-                    Remove task
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <router-link :to="{ name: 'kanban' }" class="rounded-md text-gray-300 hover:text-white focus:outline-none">
-            <span class="sr-only">Close panel</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-x stroke-current text-gray-500"
-            >
-              <line x1="18" y1="6" x2="6" y2="18"></line>
-              <line x1="6" y1="6" x2="18" y2="18"></line>
-            </svg>
-          </router-link>
-        </div>
-        <div class="flex items-center justify-between">
-          <div :class="`rounded px-3 py-1 bg-${column.board.color}-500 text-xs font-medium tracking-wider text-white`">
-            {{ column.board.text }}
-          </div>
-          <div class="flex flex-row">
-            <div
-              :class="`rounded-tl rounded-bl px-3 py-1 ${task.priority.background.color} text-xs font-medium text-white`"
-            >
-              {{ task.priority.text }}
-            </div>
-            <div
-              :class="`rounded-tr rounded-br px-3 py-1 ${task.type.background.color} text-xs font-medium text-white`"
-            >
-              {{ task.type.text }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </template>
-    <template #main>
-      <div class="flex flex-col py-4 px-4">
-        <div class="flex items-start justify-between">
+        <div class="flex items-start justify-between pb-4">
           <div>
             <div class="mb-1 text-lg text-gray-800 font-medium tracking-wide">
               {{ task.title }}
             </div>
-            <div class="text-xs text-gray-400">
-              {{ task.date | taskDate }}
+            <div class="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-calendar w-4 h-4 stroke-current text-gray-400"
+              >
+                <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                <line x1="16" y1="2" x2="16" y2="6"></line>
+                <line x1="8" y1="2" x2="8" y2="6"></line>
+                <line x1="3" y1="10" x2="21" y2="10"></line>
+              </svg>
+              <div class="ml-2 text-xs text-gray-400 font-medium">{{ task.date | taskDate }}</div>
             </div>
           </div>
           <div class="flex flex-row">
@@ -117,13 +40,90 @@
             </div>
           </div>
         </div>
-        <div class="py-4 text-md text-gray-500">
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex flex-row pr-4">
+              <div
+                :class="`rounded-tl rounded-bl px-3 py-1 ${task.priority.background.color} text-xs font-medium text-white`"
+              >
+                {{ task.priority.text }}
+              </div>
+              <div
+                :class="`rounded-tr rounded-br px-3 py-1 ${task.type.background.color} text-xs font-medium text-white`"
+              >
+                {{ task.type.text }}
+              </div>
+            </div>
+            <div
+              :class="`rounded px-3 py-1 bg-${column.board.color}-500 text-xs font-medium tracking-wider text-white`"
+            >
+              {{ column.board.text }}
+            </div>
+          </div>
+          <div class="relative text-left">
+            <div>
+              <button
+                id="options-menu"
+                type="button"
+                :class="`inline-flex justify-center px-2 py-1 ${
+                  taskOptionsToggle ? 'bg-gray-100' : ''
+                } hover:bg-gray-100 focus:outline-none`"
+                aria-haspopup="true"
+                aria-expanded="true"
+                @click="taskOptionsToggle = !taskOptionsToggle"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  :class="`feather feather-more-horizontal w-5 h-5 stroke-current text-gray-700`"
+                >
+                  <circle cx="12" cy="12" r="1"></circle>
+                  <circle cx="19" cy="12" r="1"></circle>
+                  <circle cx="5" cy="12" r="1"></circle>
+                </svg>
+              </button>
+            </div>
+            <div
+              v-if="taskOptionsToggle"
+              class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+            >
+              <div class="p-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                <button
+                  class="rounded block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-800 text-left"
+                  role="menuitem"
+                  @click="taskOptionsToggle = !taskOptionsToggle"
+                >
+                  Edit task
+                </button>
+                <button
+                  class="rounded block w-full px-4 py-2 text-sm text-red-700 hover:bg-gray-100 hover:text-red-800 text-left"
+                  role="menuitem"
+                  @click="taskOptionsToggle = !taskOptionsToggle"
+                >
+                  Remove task
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #main>
+      <div class="flex flex-col py-4 px-4">
+        <div class="text-md text-gray-500">
           {{ task.description }}
         </div>
       </div>
     </template>
     <template #footer>
-      <div class="flex flex-row pb-4 px-4 mt-auto pt-3">
+      <div class="flex-1 flex flex-row max-w-1/2">
         <button
           v-if="!isFirstBoard"
           type="button"
